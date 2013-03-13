909ff2be9fa3979bd766bc6d8c1e3932b1101ca3
Luke Palmer <luqui@clozure.com>
2013-03-13 16:43:15 -0600

    A litte more refinement.

--- class.hs
+++ class.hs
@@ -1,12 +1,14 @@
-{-# LANGUAGE ConstraintKinds, RankNTypes, MultiParamTypeClasses, FunctionalDependencies, DeriveFunctor, ScopedTypeVariables, FlexibleContexts, UndecidableInstances, PolyKinds, GADTs, FlexibleInstances, OverlappingInstances #-}
+{-# LANGUAGE ConstraintKinds, RankNTypes, MultiParamTypeClasses, FunctionalDependencies, DeriveFunctor, ScopedTypeVariables, FlexibleContexts, UndecidableInstances, PolyKinds, GADTs, FlexibleInstances, OverlappingInstances, TypeOperators, KindSignatures #-}
 
 import qualified Data.Proxy as P
 import Data.Monoid
 import Data.Reflection
+import Data.Functor.Identity
 
 data Proxy a = Proxy
 
--- algebra library
+-- F-Algebra library
+-- -----------------
 newtype Algebra f a = Algebra { getAlgebra :: f a -> a }
 
 newtype W s a = W { getW :: a }
@@ -26,7 +28,12 @@ mReify a cont = reify a (\(P.Proxy :: P.Proxy s) -> cont (W :: forall b. b -> W 
 wAlg :: forall f c a s. (Reifies s (Algebra f a), FAlgebra f c) => Proxy c -> Algebra f (W s a)
 wAlg _ = isoAlg W getW (reflect (Proxy :: Proxy s))
 
--- monoid support library
+
+data ((f :: * -> *) :+: (g :: * -> *)) a = InL (f a) | InR (g a)
+    deriving (Functor)
+
+-- Monoid support library
+-- ----------------------
 data MonoidF r 
     = MEmpty
     | MAppend r r
@@ -34,6 +41,9 @@ data MonoidF r
 
 class AMonoid m where
     monoidAlg :: Algebra MonoidF m
+
+instance FAlgebra MonoidF AMonoid where
+    toAlgebra _ = monoidAlg
 
 -- XXX questionable
 instance AMonoid m => Monoid m where
@@ -65,18 +75,74 @@ dualMonoid alg = mReify alg $ \w -> isoAlg (getW . getDual) (Dual . w) (toAlgebr
 endoMonoid :: Algebra MonoidF (a -> a)
 endoMonoid = isoAlg appEndo Endo (toAlgebra monoidT)
 
+anyMonoid :: Algebra MonoidF Bool
+anyMonoid = isoAlg getAny Any (toAlgebra monoidT)
+
+allMonoid :: Algebra MonoidF Bool
+allMonoid = isoAlg getAll All (toAlgebra monoidT)
+
 -- ...
 
--- example
+-- Num support library
+-- -------------------
+data NumF r 
+    = Plus r r
+    | Times r r 
+    | Minus r r
+    | Negate r
+    | Abs r
+    | Signum r
+    | FromInteger Integer
+    deriving (Functor)
 
-data SumProduct a = SumProduct a a
-    deriving (Show)
+class ANum a where
+    numAlg :: Algebra NumF a
 
-instance (Num a) => AMonoid (SumProduct a) where
-    monoidAlg = isoAlg (\(a,b) -> SumProduct a b) (\(SumProduct a b) -> (a,b)) 
-              $ pairAlg plusMonoid timesMonoid
+instance FAlgebra NumF ANum where
+    toAlgebra _ = numAlg
 
-sp :: a -> SumProduct a
-sp x = SumProduct x x
+instance ANum a => Num a where
+    x + y         = getAlgebra numAlg (Plus x y)
+    x * y         = getAlgebra numAlg (Times x y)
+    x - y         = getAlgebra numAlg (Minus x y)
+    negate x      = getAlgebra numAlg (Negate x)
+    abs x         = getAlgebra numAlg (Abs x)
+    signum x      = getAlgebra numAlg (Signum x)
+    fromInteger i = getAlgebra numAlg (FromInteger i)
 
-test = sp 3 `mappend` sp 4
+instance (Reifies s (Algebra NumF a)) => ANum (W s a) where
+    numAlg = wAlg (Proxy :: Proxy Num)
+
+instance FAlgebra NumF Num where
+    toAlgebra _ = Algebra go
+        where
+        go (Plus x y)      = x + y
+        go (Times x y)     = x * y
+        go (Minus x y)     = x - y
+        go (Negate x)      = negate x
+        go (Abs x)         = abs x
+        go (Signum x)      = signum x
+        go (FromInteger i) = fromInteger i
+
+numT :: Proxy Num
+numT = Proxy
+
+numInteger :: Algebra NumF Integer
+numInteger = toAlgebra numT
+
+-- Example Usage
+-- -------------
+
+-- composing instances
+test :: (Integer, Integer)
+test = mReify (pairAlg plusMonoid timesMonoid) $ \w -> getW $ w (3,3) `mappend` w (4,4)
+
+-- multiple classes
+test2 :: Integer
+test2 = mReify plusMonoid $ \plus -> 
+         mReify timesMonoid $ \times -> 
+          getW $ times (getW $ plus 1 `mappend` plus 1) 
+                     `mappend` 
+                 times (getW $ plus 2 `mappend` plus 2)
+
+
