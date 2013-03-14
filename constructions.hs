b082d359a1feb847c181d7552b1bed1c5c9a68cb
Luke Palmer <luqui@clozure.com>
2013-03-14 12:08:50 -0600

    Massive categorization of constructions.

--- constructions.hs
+++ constructions.hs
@@ -1,19 +1,102 @@
-{-# LANGUAGE TupleSections, PolyKinds, ExistentialQuantification, RankNTypes, TypeOperators, DeriveFunctor, KindSignatures, ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections, PolyKinds, DataKinds, ExistentialQuantification, RankNTypes, TypeOperators, DeriveFunctor, KindSignatures, ScopedTypeVariables, MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances, EmptyDataDecls, GADTs #-}
 
-import Control.Arrow
+import Prelude hiding (id, (.), fst, snd)
+import qualified Prelude
 import Control.Applicative
 import Data.Traversable
 import Data.Functor.Identity
+import Data.Void (Void, absurd)
 import Control.Lens (Lens', (^.), set)
+
+
+class Category hom where
+    id :: hom a a
+    (.) :: hom b c -> hom a b -> hom a c
+
+instance Category (->) where
+    id = Prelude.id
+    (.) = (Prelude..)
+
+class (Category hom) => Products hom p | hom -> p where
+    (&&&) :: hom a b -> hom a c -> hom a (p b c)
+    fst :: hom (p a b) a
+    snd :: hom (p a b) b
+
+instance Products (->) (,) where
+    (f &&& g) x = (f x, g x)
+    fst = Prelude.fst
+    snd = Prelude.snd
+
+class (Category hom) => Coproducts hom p | hom -> p where
+    (|||) :: hom a c -> hom b c -> hom (p a b) c
+    inl   :: hom a (p a b)
+    inr   :: hom b (p a b)
+
+instance Coproducts (->) Either where
+    (f ||| g) (Left x) = f x
+    (f ||| g) (Right y) = g y
+    inl = Left
+    inr = Right
+
+class (Category hom) => Initial hom i | hom -> i where
+    initial :: hom i a
+
+instance Initial (->) Void where
+    initial = absurd
+
+class (Category hom) => Terminal hom t | hom -> t where
+    terminal :: hom a t
+
+instance Terminal (->) () where
+    terminal = const ()
+
+
+infixl 1 %
+newtype ((f :: * -> *) :~> (g :: * -> *)) = NT { (%) :: forall a. f a -> g a }
+
+instance Category (:~>) where
+    id = NT id
+    NT g . NT f = NT (g . f)
+
+data ((f :: * -> *) :*: (g :: * -> *)) x = Pair (f x) (g x)
+
+instance (Functor f, Functor g) => Functor (f :*: g) where
+    fmap f (Pair x y) = Pair (fmap f x) (fmap f y)
+
+data ((f :: * -> *) :+: (g :: * -> *)) x = InL (f x) | InR (g x)
+
+instance (Functor f, Functor g) => Functor (f :+: g) where
+    fmap f (InL x) = InL (fmap f x)
+    fmap f (InR y) = InR (fmap f y)
+
+instance Products (:~>) (:*:) where
+    NT f &&& NT g = NT (\x -> Pair (f x) (g x))
+    fst = NT (\(Pair x _) -> x)
+    snd = NT (\(Pair _ y) -> y)
+
+coprodToEither :: (f :+: g) x -> Either (f x) (g x)
+coprodToEither (InL x) = Left x
+coprodToEither (InR y) = Right y
+    
+instance Coproducts (:~>) (:+:) where
+    NT f ||| NT g = NT ((f ||| g) . coprodToEither)
+    inl = NT InL
+    inr = NT InR
+
+instance Initial (:~>) (Const Void) where
+    initial = NT (\(Const x) -> absurd x)
+
+instance Terminal (:~>) (Const ()) where
+    terminal = NT (const (Const ()))
 
 type Algebra f a = f a -> a
 type Coalgebra f a = a -> f a
 
 pairAlg :: (Functor f) => Algebra f a -> Algebra f b -> Algebra f (a,b)
-pairAlg alga algb = alga . fmap fst &&& algb . fmap snd
+pairAlg alga algb = (alga . fmap fst) &&& (algb . fmap snd)
 
 eitherCoalg :: (Functor f) => Coalgebra f a -> Coalgebra f b -> Coalgebra f (Either a b)
-eitherCoalg coalga coalgb = fmap Left . coalga ||| fmap Right . coalgb
+eitherCoalg coalga coalgb = (fmap Left . coalga) ||| (fmap Right . coalgb)
 
 
 readerAlg :: (Functor f) => Algebra f a -> Algebra f (r -> a)
@@ -39,18 +122,43 @@ sigmaCoalg :: (Functor f) => Pi (RealCoalgebra f :. g) -> Coalgebra f (Sigma g)
 sigmaCoalg coalg (Sigma gy) = fmap Sigma (getRC (getCompose (getPi coalg)) gy)
 
 
-data ((f :: * -> *) :*: (g :: * -> *)) x = Pair { proj1 :: f x, proj2 :: g x }
-    deriving (Functor)
 
-data ((f :: * -> *) :+: (g :: * -> *)) x = InL (f x) | InR (g x)
-    deriving (Functor)
+
+newtype Alg a f g = AlgHom { getAlgHom :: Algebra f a -> Algebra g a }
+newtype Coalg a f g = CoalgHom { getCoalgHom :: Coalgebra f a -> Coalgebra g a }
+
+instance Category (Alg a) where
+    id = AlgHom id
+    AlgHom g . AlgHom f = AlgHom (g . f)
+
+instance Category (Coalg a) where
+    id = CoalgHom id
+    CoalgHom g . CoalgHom f = CoalgHom (g . f)
 
 coprodAlg :: Algebra f a -> Algebra g a -> Algebra (f :+: g) a
 coprodAlg falg _ (InL fa) = falg fa
 coprodAlg _ galg (InR ga) = galg ga
 
+instance Products (Alg a) (:+:) where
+    AlgHom f &&& AlgHom g = AlgHom (\x -> coprodAlg (f x) (g x))
+    fst = AlgHom (. (inl %))
+    snd = AlgHom (. (inr %))
+
+instance Terminal (Alg a) (Const Void) where
+    terminal = AlgHom (\_ (Const x) -> absurd x) 
+
+
 prodCoalg :: Coalgebra f a -> Coalgebra g a -> Coalgebra (f :*: g) a
 prodCoalg fcoalg gcoalg a = Pair (fcoalg a) (gcoalg a)
+
+instance Products (Coalg a) (:*:) where
+    CoalgHom f &&& CoalgHom g = CoalgHom (\x -> prodCoalg (f x) (g x))
+    fst = CoalgHom ((fst %) .)
+    snd = CoalgHom ((snd %) .)
+
+instance Terminal (Coalg a) (Const ()) where
+    terminal = CoalgHom (\_ _ -> Const ())
+    
 
 class (Functor f) => Costrong f where
     costrength :: f (Either a b) -> Either a (f b)
